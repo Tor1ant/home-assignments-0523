7d105c1bd71ea4ba2ccffa36df8c54daab19dbb8
Vitalii M. Naumov <hbvit7zoom@gmail.com>
2023-05-28 18:25:24 +0300

    Fix build

--- lesson02/src/test/java/ru/sberbank/jd/lesson02/NodImplTest.java
+++ lesson02/src/test/java/ru/sberbank/jd/lesson02/NodImplTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.sberbank.jd.lesson02.exceptions.NodException;
 
+/**
+ * Класс для тестирования класса {@link NodImpl}.
+ */
 public class NodImplTest {
 
     private final Nod nod = new NodImpl();
@@ -26,55 +29,55 @@ public class NodImplTest {
 
     @DisplayName("проверка MIN и произвольного  аргумента")
     @Test
-    public void testCalculateFirstMINAndSecond12() {
+    public void testCalculateFirstMinAndSecond12() {
         assertEquals(4, nod.calculate(Integer.MIN_VALUE, 12));
     }
 
     @DisplayName("проверка MAX и произвольного  аргумента")
     @Test
-    public void testCalculateFirstMAXAndSecond12() {
+    public void testCalculateFirstMaxAndSecond12() {
         assertEquals(1, nod.calculate(Integer.MAX_VALUE, 12));
     }
 
     @DisplayName("проверка произвольного  аргумента и MAX")
     @Test
-    public void testCalculateFirst12AndSecondMAX() {
+    public void testCalculateFirst12AndSecondMax() {
         assertEquals(1, nod.calculate(12, Integer.MAX_VALUE));
     }
 
     @DisplayName("проверка аргументов MAX, 0")
     @Test
-    public void testCalculateFirstNumberMAXAndSecondNumber0() {
+    public void testCalculateFirstNumberMaxAndSecondNumber0() {
         assertEquals(Integer.MAX_VALUE, nod.calculate(Integer.MAX_VALUE, 0));
     }
 
     @DisplayName("проверка аргументов 0, MAX")
     @Test
-    public void testCalculateFirstNumberOAndSecondNumberMax() {
+    public void testCalculateFirstNumberZeroAndSecondNumberMax() {
         assertEquals(Integer.MAX_VALUE, nod.calculate(0, Integer.MAX_VALUE));
     }
 
     @DisplayName("проверка аргументов MIN,0")
     @Test
-    public void testCalculateFirstNumberMINAndSecondNumber0() {
+    public void testCalculateFirstNumberMinAndSecondNumber0() {
         assertEquals(Integer.MIN_VALUE, nod.calculate(Integer.MIN_VALUE, 0));
     }
 
     @DisplayName("проверка аргументов 0,MIN")
     @Test
-    public void testCalculateFirstNumber0AndSecondNumberMIN() {
+    public void testCalculateFirstNumber0AndSecondNumberMin() {
         assertEquals(Integer.MIN_VALUE, nod.calculate(0, Integer.MIN_VALUE));
     }
 
     @DisplayName("проверка аргументов MIN,MIN")
     @Test()
-    public void testCalculateFirstNumberMINAndSecondNumberMIN() {
+    public void testCalculateFirstNumberMinAndSecondNumberMin() {
         Assertions.assertThrows(NodException.class, () -> nod.calculate(Integer.MIN_VALUE, Integer.MIN_VALUE));
     }
 
     @DisplayName("проверка аргументов MAX,MAX")
     @Test
-    public void testCalculateFirstNumberMAXAndSecondNumberMAX() {
+    public void testCalculateFirstNumberMaxAndSecondNumberMax() {
         assertEquals(Integer.MAX_VALUE, nod.calculate(Integer.MAX_VALUE, Integer.MAX_VALUE));
     }
 }
