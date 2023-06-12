da7630632d4f8243cade3618779215aeabd024b4
Vyacheslav <torlant312@yandex.ru>
2023-06-12 15:52:12 +0300

    задание lesson10 реализовано

--- lesson10/src/main/java/ru/sberbank/jd/lesson10/service/stats/StatsServiceImpl.java
+++ lesson10/src/main/java/ru/sberbank/jd/lesson10/service/stats/StatsServiceImpl.java
@@ -65,7 +65,6 @@ public class StatsServiceImpl implements StatsService {
                         countries.put(country.getName(), country);
                     }
                 }));
-
         return countries;
     }
 
