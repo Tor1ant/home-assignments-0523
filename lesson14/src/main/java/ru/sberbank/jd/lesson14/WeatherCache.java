5d075a3c263cd439dd41424c27bb82e8ddf79b93
Vyacheslav <torlant312@yandex.ru>
2023-06-24 16:53:55 +0300

    задание lesson14 реализовано

--- lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherCache.java
+++ lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherCache.java
@@ -1,44 +1,52 @@
 package ru.sberbank.jd.lesson14;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Weather cache.
  */
+@RequiredArgsConstructor
 public class WeatherCache {
 
     private final Map<String, WeatherInfo> cache = new HashMap<>();
+
     private final WeatherProvider weatherProvider;
 
     /**
-     * Constructor.
-     *
-     * @param weatherProvider - weather provider
-     */
-    public WeatherCache(WeatherProvider weatherProvider) {
-        this.weatherProvider = weatherProvider;
-    }
-
-    /**
-     * Get ACTUAL weather info for current city or null if current city not found.
-     * If cache doesn't contain weather info OR contains NOT ACTUAL info then we should download info
-     * If you download weather info then you should set expiry time now() plus 5 minutes.
-     * If you can't download weather info then remove weather info for current city from cache.
+     * Get ACTUAL weather info for current city or null if current city not found. If cache doesn't contain weather info
+     * OR contains NOT ACTUAL info then we should download info If you download weather info then you should set expiry
+     * time now() plus 5 minutes. If you can't download weather info then remove weather info for current city from
+     * cache.
      *
      * @param city - city
      * @return actual weather info
      */
-    public WeatherInfo getWeatherInfo(String city) {
-        // should be implemented
-        return null;
+    public synchronized WeatherInfo getWeatherInfo(String city) {
+        WeatherInfo cachedWeatherInfo = cache.get(city);
+        if (cachedWeatherInfo != null
+                && cachedWeatherInfo.getExpiryTime() != null
+                && cachedWeatherInfo.getExpiryTime().isAfter(LocalDateTime.now())) {
+            return cachedWeatherInfo;
+        }
+        WeatherInfo weatherInfo = weatherProvider.get(city);
+
+        if (weatherInfo == null) {
+            removeWeatherInfo(city);
+            return null;
+        } else {
+            cache.put(weatherInfo.getCity(), weatherInfo);
+            return weatherInfo;
+        }
     }
 
     /**
      * Remove weather info from cache.
      **/
     public void removeWeatherInfo(String city) {
-        // should be implemented
+        cache.remove(city);
     }
 }
 
--- lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherProvider.java
+++ lesson14/src/main/java/ru/sberbank/jd/lesson14/WeatherProvider.java
@@ -1,21 +1,56 @@
 package ru.sberbank.jd.lesson14;
 
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.SneakyThrows;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Weather provider.
  */
 public class WeatherProvider {
 
-    //private RestTemplate restTemplate = null;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     /**
-     * Download ACTUAL weather info from internet.
-     * You should call GET http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-     * You should use Spring Rest Template for calling requests
+     * Download ACTUAL weather info from internet. You should call GET
+     * <a href="http://api.openweathermap.org/data/2.5/weather?q=">...</a>{city name}&appid={API key} You should use
+     * Spring Rest Template for calling requests
      *
      * @param city - city
      * @return weather info or null
      */
+    @SneakyThrows
     public WeatherInfo get(String city) {
-        return null;
+        String api =
+                "https://api.openweathermap.org/data/2.5/weather?q=" + city
+                        + "&appid=59f8b24af4788e7aaf8eba8877e89fbe&units=metric";
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.getForEntity(api, String.class);
+        } catch (RestClientException e) {
+            throw new RuntimeException("City not found");
+        }
+        if (Objects.requireNonNull(response.getBody()).isEmpty()) {
+            return null;
+        }
+        JsonNode jsonNode = objectMapper.readTree(response.getBody());
+
+        WeatherInfo weatherInfo = new WeatherInfo();
+        weatherInfo.setCity(city);
+        weatherInfo.setDescription("Информация: " + jsonNode.get("weather").get(0).get("main"));
+        weatherInfo.setExpiryTime(LocalDateTime.now().plusMinutes(5));
+        weatherInfo.setPressure(jsonNode.get("main").get("pressure").asDouble());
+        weatherInfo.setTemperature(jsonNode.get("main").get("temp").asDouble());
+        weatherInfo.setWindSpeed(jsonNode.get("wind").get("speed").asDouble());
+        weatherInfo.setFeelsLikeTemperature(jsonNode.get("main").get("feels_like").asDouble());
+        weatherInfo.setShortDescription(" погода: " + jsonNode.get("weather").get(0).get("description"));
+        return weatherInfo;
     }
 }
